213eb92985b609d9322da99d7e9de76425ff0ee9
Mohan K <mohank@tests-iMac.local>
2023-04-26 20:01:40 +0530

    first commit

---
+++ scrollView/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  scrollView
+//
+//  Created by Mohan K on 17/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    @IBOutlet weak var textLabel:   UILabel!
+    
+    
+    
+    @IBOutlet weak var images: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        textLabel.text = "welcome to pepul"
+    }
+
+
+}
+
